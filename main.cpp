1a8b195090bbc73caf648fcbd05be357ede4a0fb
Peter Hansen <phansen@physics.umn.edu>
2017-09-16 14:04:00 -0500

    Initial commit for main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main (int argc, char *argv[])
+{
+   std::cout << "This is the hardest HIGH/LOW game ever!\n";
+
+   int n_guesses = 0;
+
+   //TODO: Write main game loop
+
+   std::cout << "You guessed my number in " << n_guesses << "guesses\n";
+
+}
