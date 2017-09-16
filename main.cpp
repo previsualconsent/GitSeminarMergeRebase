0e7ae6cccac1b85b83f1bd6a35cc8db167f6a779
Peter Hansen <phansen@physics.umn.edu>
2017-09-16 14:27:32 -0500

    Added game logic

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main (int argc, char *argv[])
    std::cout << "Can you guess it?\n";
 
    int n_guesses = 0;
+   int min = 1;
+   int max = 100;
    int guess;
 
    bool end = false;
@@ -19,6 +21,22 @@ int main (int argc, char *argv[])
       // TODO: Validate Input
       if(guess == -1) return 0;
 
+      if(min == max && guess == max)
+      {
+         std::cout << "You got it!\n";
+         end == true;
+      }
+      else if( (guess - min) > (max - guess)) 
+      {
+         std:: cout << "Too High\n";
+         max = guess - 1;
+      }
+      else
+      {
+         std:: cout << "Too Low\n";
+         min = guess + 1;
+      }
+   }
    std::cout << "You guessed my number in " << n_guesses << "guesses\n";
 
 }
