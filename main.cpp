d57cdf046616a6b49a1097b67a4705523a0c7db2
Peter Hansen <phansen@physics.umn.edu>
2017-09-16 14:23:45 -0500

    main loop framework

--- main.cpp
+++ main.cpp
@@ -3,10 +3,21 @@
 int main (int argc, char *argv[])
 {
    std::cout << "This is the hardest HIGH/LOW game ever!\n";
+   std::cout << "Enter -1 to quit\n";
+   std::cout << "I'm thinking of a integer between 1-100.\n";
+   std::cout << "Can you guess it?\n";
 
    int n_guesses = 0;
+   int guess;
 
-   //TODO: Write main game loop
+   bool end = false;
+   while(!end)
+   {
+      std::cout << "Guess my number(" << min << "-" << max << "): ";
+      std::cin >> guess;
+      n_guesses++;
+      // TODO: Validate Input
+      if(guess == -1) return 0;
 
    std::cout << "You guessed my number in " << n_guesses << "guesses\n";
 
